131c79087f3a3a5b651f23bdbf7cff8b7cb9648d
klausjensensnet <klausj@gmail.com>
2015-06-29 01:03:57 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ import scraperwiki
 from urllib import urlopen
 from BeautifulSoup import BeautifulSoup
 
-webpage = urlopen('http://johannesburg.gumtree.co.za/f-Jobs-W0QQAdTypeZ1QQCatIdZ8').read()
+webpage = urlopen('http://www.gumtree.com/search?q=boxster&tq=%7B%22i%22%3A%22boxster%22%2C%22s%22%3A%22boxster%22%2C%22p%22%3A0%2C%22t%22%3A13%7D&search_category=porsche&search_location=&distance=0.0001').read()
 
 soup = BeautifulSoup(webpage)
 # get the link to the profile page
